7045bf0c333085d79aad9b1c3ee75049dd189f82
ethan.w.foshee <skyhailer2@gmail.com>
2023-04-04 20:58:00 -0500

    Updates

---
+++ precache-manifest.9363c6918a82a9bf2752cd296abfd0a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "929dead576536dd6ab64e0e9132241fb",
+    "url": "/react-authen-work/index.html"
+  },
+  {
+    "revision": "f16c7e1c309d8ca133cc",
+    "url": "/react-authen-work/static/css/main.909db9c4.chunk.css"
+  },
+  {
+    "revision": "80ff0ee92c0fb60cb1dd",
+    "url": "/react-authen-work/static/js/2.e61ae665.chunk.js"
+  },
+  {
+    "revision": "f16c7e1c309d8ca133cc",
+    "url": "/react-authen-work/static/js/main.6bef935e.chunk.js"
+  },
+  {
+    "revision": "19b7052504039082f66f",
+    "url": "/react-authen-work/static/js/runtime~main.08a9a455.js"
+  }
+]);
